7de3197989341b4e708ee82c674d86ec43aa62b0
JonKappa <cappytwoj@gmail.com>
2023-10-09 02:19:15 -0400

    create button with alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import {useState } from "react";
 import Button from "./components/Button";
-import Alert from "./components/alert";
+import Alert from "./components/Alert";
 
 function App()
 {
+    const [alertVisable, setAlertVisiblility] = useState(false);
 
     return (
     <div>
-        <Alert>
-            Hello <span>World</span>
-        </Alert>
+        {alertVisable && <Alert onClose={() => setAlertVisiblility(false)}>Hello World Alert</Alert>}
 
-        <Button color="primary" onClick={() => console.log('Clicked')}>Hello World</Button>
+        <Button color="primary" onClick={() => setAlertVisiblility(true)}>Hello World</Button>
     </div>);
 }
 
