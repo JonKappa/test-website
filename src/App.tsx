7faaa3a5e4159d853652aa33ba7a6af601ba735b
Jonathan Capparell <cappytwoj@gmail.com>
2023-10-10 17:00:26 -0400

    initial start

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {useState } from "react";
+import Nav from "./components/Nav";
 import Button from "./components/Button";
 import Alert from "./components/Alert";
 
@@ -6,12 +7,18 @@ function App()
 {
     const [alertVisable, setAlertVisiblility] = useState(false);
 
-    return (
-    <div>
-        {alertVisable && <Alert onClose={() => setAlertVisiblility(false)}>Hello World Alert</Alert>}
+    // return (
+    // <div>
+    //     {alertVisable && <Alert onClose={() => setAlertVisiblility(false)}>Hello World Alert</Alert>}
 
-        <Button color="primary" onClick={() => setAlertVisiblility(true)}>Hello World</Button>
-    </div>);
+    //     <Button color="primary" onClick={() => setAlertVisiblility(true)}>Hello World</Button>
+    // </div>);
+
+    return (
+        <>
+            <Nav/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+function Nav()
+{
+    return (
+        <nav>
+            <ul>
+                <div id="logo"></div>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
